b919ed66320132397be52933cb9efd0cd3509c05
WorldWideGreg <greg_hehe@yahoo.fr>
2022-06-30 17:49:39 +0200

    title ok

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-  var mouseX, mouseY;
-  var ww = $(window).width();
-  var wh = $(window).height();
-  var traX, traY;
+  let mouseX, mouseY;
+  let ww = $(window).width();
+  let wh = $(window).height();
+  let traX, traY;
   $(document).mousemove(function (e) {
     mouseX = e.pageX;
     mouseY = e.pageY;
@@ -12,3 +12,93 @@ $(document).ready(function () {
     $(".title").css({ "background-position": traX + "%" + traY + "%" });
   });
 });
+
+/*
+(function () {
+  let addEvent = function (object, type, callback) {
+    if (object == null || typeof object == "undefined") return;
+    if (object.addEventListener) {
+      object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+      object.attachEvent("on" + type, callback);
+    } else {
+      object["on" + type] = callback;
+    }
+  };
+  function detectMouseMove() {
+    // Initial container width
+    let containerWidth = $(".containerFocus").outerWidth();
+    document.addEventListener("mousemove", function (e) {
+      $(".hover").css({
+        left: e.pageX,
+      });
+      $(".left-side").css({
+        left: e.pageX + 75,
+      });
+      var rightSideLeft = containerWidth - e.pageX;
+      $(".right-side").css({
+        right: rightSideLeft,
+      });
+    });
+  }
+  detectMouseMove();
+
+  function detectTouch() {
+    // Initial container width
+    let containerWidth = $(".containerFocus").outerWidth();
+    document.addEventListener("mousemove", function (e) {
+      $(".hover").css({
+        left: e.pageX,
+      });
+      $(".left-side").css({
+        left: e.pageX + 75,
+      });
+      let rightSideLeft = containerWidth - e.pageX;
+      $(".right-side").css({
+        right: rightSideLeft,
+      });
+    });
+  }
+  detectMouseMove();
+
+  function touchMove() {
+    // Initial container width
+    let containerWidth = $(".containerFocus").outerWidth();
+    document.addEventListener("touchmove", function (e) {
+      $(".hover").css({
+        left: e.pageX,
+      });
+      $(".left-side").css({
+        left: e.pageX + 75,
+      });
+      var rightSideLeft = containerWidth - e.pageX;
+      $(".right-side").css({
+        right: rightSideLeft,
+      });
+    });
+  }
+
+  touchMove();
+
+  function detectInput(inputType) {
+    // Detect container width after resizing
+    addEvent(window, "resize", function (event) {
+      var containerWidth = $(".containerFocus").outerWidth();
+      $(document).bind(inputType, function (e) {
+        $(".hover").css({
+          left: e.pageX,
+        });
+        $(".left-side").css({
+          left: e.pageX + 75,
+        });
+        let rightSideLeft = containerWidth - e.pageX;
+        $(".right-side").css({
+          right: rightSideLeft,
+        });
+      });
+    });
+  }
+
+  detectInput("mousemove");
+  detectInput("touchmove");
+})();*/
